f137da274ac478c05f0923f585e05220c962b6b4
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-11 13:41:08 +0200

    remove preloaded resources + add equip system

--- Player/Weapon.gd
+++ Player/Weapon.gd
@@ -2,15 +2,6 @@ class_name Weapon
 extends Area2D
 
 signal dmg_changed(new_damage)
-signal firesword_looted
-signal frostsword_looted
-
-export(Resource) var weapon
-export(Resource) var fire_sword
-
-
-export (bool) var has_firesword:bool = false
-export (bool) var has_frostsword:bool = false
 
 signal weapon_changed
 signal weapon_energie_changed
@@ -23,9 +14,11 @@ var reset_dmg = reset_dmg setget set_damage
 var max_weapon_energie = max_weapon_energie setget set_max_weapon_energie
 var weapon_energie= weapon_energie setget set_weapon_energie
 
+var arsenal = []
+
 func _ready():
-	if weapon != null:
-		equip_weapon(weapon)
+	arsenal = DataManager.player_data.arsenal
+	equip_weapon(arsenal[0])
 	max_weapon_energie = 0
 
 
@@ -51,7 +44,8 @@ func get_damage():
 	return damage
 
 func equip_weapon(new_weapon):
-	current_weapon = new_weapon
+	var sword = load("res://classes/items/"+new_weapon+".tres")
+	current_weapon = sword
 	reset_dmg = current_weapon.weapon_damage
 	set_damage(current_weapon.weapon_damage)
 	emit_signal("weapon_changed")
@@ -60,17 +54,11 @@ func equip_weapon(new_weapon):
 		set_weapon_energie(0)
 	else:
 		GameManager.interface.weaponEnergiePlate.visible = false
-	
 
-func loot_weapon(looted_weapon):
-	if looted_weapon == 'FireSword':
-		has_firesword = true
-		emit_signal("firesword_looted")
-	elif looted_weapon == 'FrostSword':
-		has_frostsword = true
-		emit_signal("frostsword_looted")
-	else:
-		pass
+func loot_weapon(weapon_name:String):
+	arsenal.append(String(weapon_name))
+	DataManager.player_data.arsenal = arsenal
+	equip_weapon(weapon_name)
 
 func _on_Sword_area_entered(area):
 	if !area.is_in_group('enemy_hitbox'):
