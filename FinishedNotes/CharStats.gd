193516fff0cbcd8b90c7db3dc3cb104ad9051c82
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-11 15:12:46 +0200

    Update Weapon EQ function
    function work now propertly

--- FinishedNotes/CharStats.gd
+++ FinishedNotes/CharStats.gd
@@ -1,9 +1,5 @@
 extends Control
 
-export (Resource) var firesword_item
-export (Resource) var practicesword_item
-
-
 onready var playername = $Bg/M/V/StatContainer/namevalue
 onready var health = $Bg/M/V/StatContainer/healthvalue
 onready var stamina = $Bg/M/V/StatContainer/StaminaValue
@@ -22,19 +18,38 @@ onready var next_weapon_btn = $EQWeapon/M/V/H/NextBtn
 onready var last_weapon_btn = $EQWeapon/M/V/H/BeforeBtn
 onready var weapon_desc = $EQWeapon/M/V/Desc
 
-onready var fs_icon = firesword_item.icon
-onready var ps_icon = practicesword_item.icon 
+onready var default_weapon = preload("res://classes/items/Holz-Schwert.tres")
 
 var player:Player
-var weapons = ['PracticeSword','FireSword']
+
 var max_weapons = 2
 var next_weapon
-var curr_weapon
+var curr_weapon = 0
+var next_weapon_name = ""
+
 
 func _ready():
 	next_weapon_btn.disabled = true
 	last_weapon_btn.disabled = true
-	curr_weapon = weapons[0]
+	max_weapons = DataManager.player_data.arsenal.size()
+	for weapon in max_weapons:
+		print(DataManager.player_data.arsenal[weapon-1])
+
+func show_weapon_preview():
+	curr_weapon = 0
+	player = GameManager.player
+	weaponeq_frame.visible = true
+	max_weapons = DataManager.player_data.arsenal.size()
+	print(max_weapons)
+	if max_weapons > 1:
+		next_weapon_btn.disabled = false
+		last_weapon_btn.disabled = false
+	w_name_label.text = default_weapon.name
+	old_damage_label.text = str(player.weapon.current_weapon.weapon_damage)
+	new_damage_label.text = str(default_weapon.weapon_damage)
+	weapon_icon.texture = default_weapon.icon
+	weapon_desc.text = str(default_weapon.description)
+
 
 func get_char_stats():
 	player = GameManager.player
@@ -44,18 +59,6 @@ func get_char_stats():
 	stamina.text = String(player.max_stamina)
 	amor.text = String(player.max_amor)
 	element.text = player.weapon.current_weapon.element_type
-
-func show_weapon_preview():
-	player = GameManager.player
-	weaponeq_frame.visible = true
-	weapon_icon.texture = player.weapon.current_weapon.icon
-	if player.weapon.has_firesword:
-		next_weapon_btn.disabled = false
-		last_weapon_btn.disabled = false
-	w_name_label.text = player.weapon.current_weapon.name
-	old_damage_label.text = str(player.weapon.damage)
-	new_damage_label.text = str(player.weapon.damage)
-	weapon_desc.text = str(player.weapon.current_weapon.description)
 
 
 func _on_WeaponIcon_pressed():
@@ -67,4 +70,37 @@ func _on_WeaponIcon_pressed():
 func _on_WeaponBackBtn_pressed():
 	player = GameManager.player
 	weaponeq_frame.visible = false
+	get_char_stats()
 	stat_frame.visible = true
+
+
+func _on_BeforeBtn_pressed():
+	curr_weapon += 1
+	if curr_weapon == max_weapons:
+		curr_weapon = 0
+	next_weapon_name = String(DataManager.player_data.arsenal[curr_weapon])
+	print(next_weapon_name)
+	next_weapon = load("res://classes/items/"+next_weapon_name+".tres")
+	update_weapon_eq()
+
+func update_weapon_eq():
+	weapon_icon.texture = next_weapon.icon
+	w_name_label.text = next_weapon.name
+	old_damage_label.text = str(player.weapon.get_damage())
+	new_damage_label.text = str(next_weapon.weapon_damage)
+	weapon_desc.text = str(next_weapon.description)
+
+
+func _on_NextBtn_pressed():
+	if curr_weapon == 0:
+		curr_weapon = max_weapons
+	curr_weapon -= 1
+	next_weapon_name = String(DataManager.player_data.arsenal[int(curr_weapon)])
+	print(next_weapon_name)
+	next_weapon = load("res://classes/items/"+next_weapon_name+".tres")
+	update_weapon_eq()
+
+func _on_WeaponEquipBtn_pressed():
+	player.weapon.equip_weapon(next_weapon_name)
+	update_weapon_eq()
+	get_char_stats()
