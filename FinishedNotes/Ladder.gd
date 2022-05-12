e578fdcddbeea33ea6e7cd6089d52e61dbff2f32
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-12 09:53:34 +0200

    fix climbing down bug

--- FinishedNotes/Ladder.gd
+++ FinishedNotes/Ladder.gd
@@ -17,10 +17,14 @@ func _on_Ladder_body_exited(body):
 
 func _process(delta):
 	if player!=null:
-		if player.climbing:
-			collider.set_deferred('disabled',false)
-		else:
+		if player.climbing and Input.is_action_just_pressed("ui_up"):
+			collider.set_deferred('disabled',true)
+		elif player.can_climbing and Input.is_action_just_pressed('ui_down'):
 			collider.set_deferred('disabled', true)
+		elif player.can_climbing and player.climbing and Input.is_action_pressed('ui_down'):
+			collider.set_deferred('disabled', true)
+		else:
+			collider.set_deferred('disabled', false)
 	else:
 		return
 
--- classes/Enemys/SmallGhost.gd
+++ classes/Enemys/SmallGhost.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
 
-var target
+var player: Player = null
 var boost_speed:= 260.0
 var move_speed := 160.0
 
 var max_speed := move_speed
-var velocity := Vector2(0,0)
+var velocity = Vector2.ZERO
 var last_x := 0.0
 var drag_factor := 0.1
 
@@ -16,18 +16,47 @@ func _process(delta):
 		$Body.flip_h = false
 	else:
 		$Body.flip_h = true
-	var direction :=Input.get_vector("move_l",'move_r',"ui_up",'ui_down')
-	if Input.is_action_just_pressed("attack"):
-		max_speed = boost_speed
-		get_node('Timer').start()
-	var desired_velocity :=max_speed * direction
-	var steering_vector := desired_velocity -velocity
-	velocity += steering_vector * drag_factor
-	position += velocity * delta
+#	var direction :=Input.get_vector("move_l",'move_r',"ui_up",'ui_down')
+#	if Input.is_action_just_pressed("attack"):
+#		max_speed = boost_speed
+#		get_node('Timer').start()
+#	var desired_velocity :=max_speed * direction
+#	var steering_vector = desired_velocity -velocity
+#	velocity += steering_vector * drag_factor
+#	position += velocity * delta
 	if velocity.x != 0.0:
 		last_x = velocity.x
+
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	
+	if player != null:
+		var direction = position.direction_to(player.position)
+		var desrired_velocity = max_speed * direction
+		var steering_vector = desrired_velocity - velocity
+		velocity += steering_vector * drag_factor
+		position += velocity * delta
+	else:
+		velocity = Vector2.ZERO
+	
+	velocity = velocity.normalized()
+	velocity = move_and_collide(velocity)
+	
 
 
 
 func _on_Timer_timeout():
 	move_speed = max_speed
+
+
+func _on_PlayerDetect_body_entered(body):
+	if !body.name == 'Player':
+		return
+	else:
+		player = body
+
+func _on_PlayerDetect_body_exited(body):
+	if !body.name == 'Player':
+		return
+	else:
+		player = null
