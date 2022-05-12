c66d7f87cec7f5ad68ba2a35728d07e6ede49ea7
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-12 13:25:50 +0200

    add Z-Button on mobile controller

--- FinishedNotes/UI/MobileControlls.gd
+++ FinishedNotes/UI/MobileControlls.gd
@@ -3,10 +3,22 @@ class_name MobileController
 
 
 onready var action_controller = $ActionPad
+onready var z_button = $ActionPad/ZTouchButton
 
+
+func _read():
+	GameManager.register_mobilecontroller(self)
+
+func show_z_btn():
+	z_button.show()
 
 
 func _on_XTouchButton_pressed():
 	Input.action_press("interact")
 	if GameManager.player.velocity.x > 0 or GameManager.player.velocity.x < 0:
 		Input.action_press("slide")
+
+
+func _on_ZTouchButton_pressed():
+	Input.action_press("sword_attack")
+	z_button.hide()
