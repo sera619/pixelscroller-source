f33545b65890b2eb47c70f26102e0f37413d42a6
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-13 22:28:31 +0200

    add Bodyamor funtionlity

--- Player/BodyAmor.gd
+++ Player/BodyAmor.gd
@@ -10,17 +10,35 @@ signal defense_changed
 var current_bodyamor = null
 var defense = defense setget set_defense
 
+var defense_arsenal = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if current_bodyamor == null:
-		equip_bodyamor(practice_amor)
+	defense_arsenal = DataManager.player_data.defense_arsenal
+	equip_bodyamor(DataManager.player_data.current_bodyamor)
+
+func get_defense():
+	return int(current_bodyamor.amor_defense)
+
 
 func equip_bodyamor(new_bodyamor):
-	current_bodyamor = new_bodyamor
-	set_defense(current_bodyamor.amor_defense)
-	DataManager.player_data.bodyamor = current_bodyamor
+	var new_amor = load('res://classes/items/Amor/'+String(new_bodyamor)+'.tres')
+	current_bodyamor = new_amor
+	set_defense(int(current_bodyamor.amor_defense))
+	DataManager.player_data.current_bodyamor =new_bodyamor 
 	emit_signal("bodyamor_changed")
-	
+
+func loot_amor(amor_name:String):
+	if amor_name in defense_arsenal:
+		print('>>> PlayerAmor: Bodyamor already exists in defense arsenal. Skip loot.')
+		return
+	defense_arsenal.append(String(amor_name))
+	DataManager.player_data.defense_arsenal = defense_arsenal
+	print('>>> PlayerAmor: New Bodyamor '+String(amor_name)+' added to arsenal.')
+	equip_bodyamor(amor_name)
+
+
+
 
 func set_defense(new_defense):
 	defense = int(new_defense)
