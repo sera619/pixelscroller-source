ed4b30401bdee47679d15a0849a9363e69601a52
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-14 22:13:48 +0200

    add light occulder on tilemaps

--- World/Map/Dungeonmap.gd
+++ World/Map/Dungeonmap.gd
@@ -3,18 +3,22 @@ extends MapBase
 
 
 export(String, "20.0","30.0","40.0","10.0") var light_time
+onready var light_timer = $LightTimer
 
 
-
-
-
-
-
-
+func set_timer(time):
+	light_timer.wait_time = time
+	GameManager.interface.dungeon_timer.set_time(time)
+	light_timer.start()
+	print('>>> MAP: Lights on for '+String(time)+'!')
 
 func _on_LightTimer_timeout():
 	get_tree().call_group('dungeon_light', 'lights_off')
+	print('>>> MAP: Lights off!')
 
 func _on_LightTrigger1_body_entered(body):
 	get_tree().call_group('camera', 'lights_off')
 	get_tree().call_group('dungeon_light', 'lights_on')
+	GameManager.interface.dungeon_timer.set_time(20.0)
+	light_timer.start(20.0)
+	print('>>> MAP: Lights on for '+String(20.0)+'!')
