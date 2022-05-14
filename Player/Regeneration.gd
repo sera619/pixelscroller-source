9242e748bd4625960f50ea6f4bdf598d3f5a497d
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-14 10:45:36 +0200

    Update Regeneration.gd

--- Player/Regeneration.gd
+++ Player/Regeneration.gd
@@ -34,7 +34,7 @@ func reg_amor():
 func _on_RegTimer_timeout():
 	if player.stamina != player.max_stamina:
 		player.set_stamina(player.stamina + 1)
-		print('>> PLAYER STATS: New Stamina: '+ str(player.stamina))
+		print('>>> PLAYER STATS: New Stamina: '+ str(player.stamina))
 		return
 	elif player.amor != player.max_amor:
 		player.set_amor(player.amor +1)
