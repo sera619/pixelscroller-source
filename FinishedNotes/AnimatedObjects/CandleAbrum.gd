879312d4aacec15f44056c993ceb33b7a16ab0b1
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-14 22:12:50 +0200

    remove prints & 2 light 2d

--- FinishedNotes/AnimatedObjects/CandleAbrum.gd
+++ FinishedNotes/AnimatedObjects/CandleAbrum.gd
@@ -11,11 +11,11 @@ func lights_on():
 	for i in get_children():
 		i.enabled = true
 		self.play('On')
-		print('>>> GAMEOBJECT: <CandleAbrum> Lights ON!')
+	#print('>>> GAMEOBJECT: <CandleAbrum> Lights ON!')
 
 
 func lights_off():
 	for i in get_children():
 		i.enabled = false
 		self.play('Off')
-		print('>>> GAMEOBJECT: <CandleAbrum> Lights OFF!')
+	#print('>>> GAMEOBJECT: <CandleAbrum> Lights OFF!')
