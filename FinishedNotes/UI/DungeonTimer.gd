5dc5d514ab9ac5d31e4ee43379186a43d92b05d7
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-14 22:30:52 +0200

    reformat display time

--- FinishedNotes/UI/DungeonTimer.gd
+++ FinishedNotes/UI/DungeonTimer.gd
@@ -26,7 +26,7 @@ func _process(delta):
 	time_now = OS.get_unix_time()
 	if max_time > time_now:
 		time_left = max_time -time_now
-		var display_time = '%0d'% time_left
+		var display_time = '%02d'% time_left
 		text_label.bbcode_text = "[center]"+String(display_time)+"[/center]"
 		if !self.visible:
 			self.show()
