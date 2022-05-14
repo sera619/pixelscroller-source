08b30f9bf5cc2be9f18f90016cdc6a4174551310
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-14 11:12:42 +0200

    remove timerlabel

--- FinishedNotes/UI/DebuffBar.gd
+++ FinishedNotes/UI/DebuffBar.gd
@@ -3,7 +3,6 @@ extends Control
 
 onready var slow_debuff_frame = $M/H/SlowDebuff
 onready var icon = $M/H/SlowDebuff/Icon
-onready var tween = $Tween
 onready var slow_timer =$M/H/SlowDebuff/SlowTimer
 onready var animPlayer = $AnimationPlayer
 
@@ -13,16 +12,13 @@ var is_active: bool = false
 
 func start_slow(slow_time:float):
 	self.show()
+	slow_debuff_frame.show()
 	animPlayer.play("slow_debuff")
-	var seconds = slow_time
-	var milliseconds = slow_time / 60
-	var time_text = "%0d:%0d" % [seconds,milliseconds]
-	slow_debuff_frame.get_node("Time").text = String(time_text)
 	slow_timer.start(slow_time)
 
 func hide_debuff():
 	if slow_debuff_frame.visible:
-		slow_debuff_frame.visible = false
+		slow_debuff_frame.hide()
 
 
 func _on_SlowTimer_timeout():
