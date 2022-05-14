ed938f07acf9115c9150b9c96a976ecb1776f46c
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-14 10:44:28 +0200

    Create LootPopup for special loot drops

---
+++ FinishedNotes/UI/LootPopup.gd
@@ -0,0 +1,27 @@
+extends Popup
+
+onready var text_label = $BG/M/V/Main
+onready var sfx = $ButtonSFX
+onready var header_label = $BG/M/V/Header
+onready var item_icon = $BG/M/V/TextureRect
+var header_base = "Neue %s erhalten!"
+var main_base = "[center]Du hast [color=red]'%s'[/color] gefunden."
+
+var main_text = "[center]Test Text"
+
+
+func set_loot_text(item_type, itemname:String, new_icon):
+	var full_text = main_base % itemname
+	var full_header = header_base % item_type
+	text_label.bbcode_text = full_text + "\n" + main_text
+	header_label.text = full_header
+	item_icon.texture = new_icon
+	self.popup_centered()
+
+
+
+func _on_OkBtn_pressed():
+	get_tree().paused = false
+	sfx.play()
+	yield(sfx,"finished")
+	self.call_deferred('queue_free')
